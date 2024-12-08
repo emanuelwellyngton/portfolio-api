f24f74239bc6624bd8f91b3b53c1680ce11e290c
Emanuel Wellyngton <emanuelwellygton@gmail.com>
2024-12-08 10:15:07 -0300

    feat: users table update and user-verification-code table created

---
+++ src/main/resources/db/migration/V2__recreate-table-users.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id BIGINT AUTO_INCREMENT UNIQUE,
+    login VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL UNIQUE,
+    fristName VARCHAR(255) NOT NULL,
+    lastName VARCHAR(255) NOT NULL,
+    confirmed BIT(1) DEFAULT 0,
+    signUpAt DATETIME DEFAULT NOW(),
+
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE `users-verification-code` (
+    code VARCHAR(255) UNIQUE NOT NULL,
+    user VARCHAR(255) NOT NULL,
+    expiresAt DATETIME DEFAULT DATE_ADD(NOW(), INTERVAL 2 HOUR),
+    valid BIT(1) DEFAULT 1,
+    PRIMARY KEY(code)
+)
