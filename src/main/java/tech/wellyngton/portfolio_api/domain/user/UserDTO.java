02327adb5b87aebdf327b46d65446a1ed35c738e
Emanuel Wellyngton <emanuelwellygton@gmail.com>
2024-12-15 11:52:36 -0300

    feat: adding method arguments validation do the user dto fields

--- src/main/java/tech/wellyngton/portfolio_api/domain/user/UserDTO.java
+++ src/main/java/tech/wellyngton/portfolio_api/domain/user/UserDTO.java
@@ -1,9 +1,24 @@
 package tech.wellyngton.portfolio_api.domain.user;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.validation.annotation.Validated;
+
 public record UserDTO(
+        @NotNull
+        @Email(message = "invalid_email_format")
         String login,
+
+        @NotNull
+        @Length(min = 8, message = "password_min")
         String password,
+
+        @NotNull
         String fristName,
+
+        @NotNull
         String lastName
         ) {
 }
