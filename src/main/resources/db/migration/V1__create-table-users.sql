65965e4390ef1bd3e8216df56f9c48d90434431b
Emanuel Wellyngton <emanuelwellygton@gmail.com>
2024-12-15 11:43:07 -0300

    feat: recreating users table migration

--- src/main/resources/db/migration/V1__create-table-users.sql
+++ src/main/resources/db/migration/V1__create-table-users.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users (
     id BIGINT AUTO_INCREMENT UNIQUE,
     login VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
     active BIT(1),
 
     PRIMARY KEY(id)
