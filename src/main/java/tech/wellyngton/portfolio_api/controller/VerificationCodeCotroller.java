ab3fc4b0835b9ca565600e3356b08b3e6e99d644
Emanuel Wellyngton <emanuelwellygton@gmail.com>
2024-12-29 12:50:51 -0300

    feat(VerificationCodeController): changing the endpoint to create a new code and changing the passage of login in the registration method and the passage of code in the verifyCode method do request param

--- src/main/java/tech/wellyngton/portfolio_api/controller/VerificationCodeCotroller.java
+++ src/main/java/tech/wellyngton/portfolio_api/controller/VerificationCodeCotroller.java
@@ -5,7 +5,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-import tech.wellyngton.portfolio_api.domain.user.LoginDTO;
 import tech.wellyngton.portfolio_api.domain.user.UserEntity;
 import tech.wellyngton.portfolio_api.domain.verification_code.VerificationCodeEntity;
 import tech.wellyngton.portfolio_api.domain.verification_code.VerificationCodeRepository;
@@ -25,19 +24,19 @@ public class VerificationCodeCotroller {
     @Autowired
     private VerificationCodeService verificationCodeService;
 
-    @PostMapping
+    @PostMapping("/new")
     @Transactional
     //TODO Verificar que usuário não está confirmado
-    public VerificationCodeEntity registration(@RequestBody LoginDTO dto){
+    public VerificationCodeEntity registration(@RequestParam String login){
         //TODO Definir código existente do usuário como inválido
-        UserEntity user = userService.getUserByLogin(dto.login());
+        UserEntity user = userService.getUserByLogin(login);
         return codeService.codeRegister(user);
     }
 
-    @PostMapping("/{code}")
+    @PostMapping
     @Transactional
     @PreAuthorize("@verificationCodeService.isCodeValid(#code)")
-    public ResponseEntity verifyCode(@PathVariable String code){
+    public ResponseEntity verifyCode(@RequestParam String code){
         var codeEntity = verificationCodeService.getCode(code);
         codeEntity.getUser().setConfirmed(true);
         codeEntity.setValid(false);
